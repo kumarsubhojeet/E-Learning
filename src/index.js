86a4586e803d63e40ed36c61782e850b7bba28a9
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2021-11-17 13:01:33 +0530

    changes1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 import 'antd/dist/antd.css'; 
 import './CSS/index.css'
 import './CSS/Crousal.css'
+
+import {BrowserRouter} from "react-router-dom"
+
 ReactDOM.render(
-  <>
+  <BrowserRouter>
     <App />
-  </>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
